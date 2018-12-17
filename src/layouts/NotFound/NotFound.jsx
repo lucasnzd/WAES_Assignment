2f8689b557240495c9929e1f0fd4b35f9ccd1b54
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-17 05:33:59 -0300

    Edited Layout files with jsx extension

---
+++ src/layouts/NotFound/NotFound.jsx
@@ -0,0 +1,22 @@
+import React, { Component }from 'react';
+import Header from '../../components/Header';
+import Section from '../../components/Section';
+import { Link } from 'react-router-dom';
+import styles from './notFound.module.css';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <main>
+                <Header />
+                <Section title="Sorry, it wasn't possible to reach that url :(">
+                    <div className={styles.link}>
+                        <Link to="/">Go back to the Highlighter app</Link>
+                    </div>
+                </Section>
+            </main>
+        );
+    }
+}
+
+export default NotFound;
