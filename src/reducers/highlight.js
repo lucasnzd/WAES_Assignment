a9aabc5adbbc9bd5278177c283875ce6de6fa634
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 11:57:42 -0300

    Created highlight reducer

---
+++ src/reducers/highlight.js
@@ -0,0 +1,34 @@
+import { HIGHLIGHT_TEXT, ADD_NOTE, ADD_TEXT } from '../actions/actionTypes';
+
+const initialState = {
+    textInput: '.....',
+    selections: [],
+    notes: []
+};
+
+const highlightReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case HIGHLIGHT_TEXT: {
+            return {
+                ...state,
+                selections: [ ...state.selections, action.selection ]
+            };
+        }
+        case ADD_NOTE: {
+            return {
+                ...state,
+                notes: [ ...state.notes, action.note ]
+            };
+        }
+        case ADD_TEXT: {
+            return {
+                ...state,
+                textInput: action.text
+            };
+        }
+        default:
+            return state;
+    }
+};
+
+export default highlightReducer; 
