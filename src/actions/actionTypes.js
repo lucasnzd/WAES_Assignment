7232e11c6122e50a2821f462681fd794b3b88c61
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 11:58:05 -0300

    Created actionTypes.js

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,20 @@
+// Highlight_actions
+export const ADD_TEXT = 'ADD_TEXT';
+export const ADD_NOTE = 'ADD_NOTE';
+export const HIGHLIGHT_TEXT = 'HIGHLIGHT_TEXT';
+
+// Filter_actions
+export const FILTER_BY = 'FILTER_BY';
+export const SHOW_ALL = 'FILTER_BY';
+
+// Color_actions
+export const SET_CURRENT_COLOR = 'SET_CURRENT_COLOR';
+
+export default {
+    ADD_TEXT,
+    ADD_NOTE,
+    FILTER_BY,
+    HIGHLIGHT_TEXT,
+    SET_CURRENT_COLOR,
+    SHOW_ALL
+};
