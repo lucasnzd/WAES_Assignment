7397f86c60033195f592aa7681621e80b59b9e1f
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 19:28:16 -0300

    Added to TextEditor components the logic for the props

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -2,14 +2,33 @@ import React, { Component }from 'react';
 import textEditorProps from './textEditor.props';
 import MarkersBar from '../MarkersBar';
 import Editor from '../Editor';
-import styles from './textEditor.module.css'; 
+import styles from './textEditor.module.css';
 
 class TextEditor extends Component {
+
+    getCurrentColor = () => {
+        const highlightColor  = this.props.highlightColor;
+        const options = this.props.markersColors;
+        if (options[0] === highlightColor) return 'firstColor';
+        if (options[1] === highlightColor) return 'secondColor';
+        if (options[2] === highlightColor) return 'thirdColor';
+    }
+
     render() {
         return (
             <div className={styles.editorContainer}>
-                <MarkersBar />
-                <Editor />
+                <MarkersBar
+                    markersColors={this.props.markersColors}
+                    onColorChange={this.props.onColorChange}
+                />
+                <Editor
+                    currentColor={this.getCurrentColor()}
+                    currentText={this.props.currentText}
+                    highlightColor={this.props.highlightColor}
+                    onSelection={this.props.onSelection}
+                    handleSelection={this.props.handleSelection}
+                    onTextInput={this.props.onTextInput}
+                />
             </div>
         );
     }
