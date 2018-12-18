d5ca03c98946ca537afeb5e56e6820d65ee994f8
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 11:55:57 -0300

    updated NotFound.test

--- src/test/unit/layouts/NotFound.test.js
+++ src/test/unit/layouts/NotFound.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
 import NotFound from '../../../layouts/NotFound';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NotFound />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <MemoryRouter>
+            <NotFound />
+        </MemoryRouter>
+    , div);
+    ReactDOM.unmountComponentAtNode(div);
 });
