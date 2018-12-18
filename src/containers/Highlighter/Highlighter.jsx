0f5c98d3a65bb207ea4f653507b13f4aaa8cd73d
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 19:27:07 -0300

    Connected Highlighter container to redux and manage props

--- src/containers/Highlighter/Highlighter.jsx
+++ src/containers/Highlighter/Highlighter.jsx
@@ -1,5 +1,10 @@
 import React, { Component }from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import {
+    addNote,
+    addText,
+    highlightText,
+    setCurrentColor } from '../../actions';
 import Section from '../../components/Section';
 import TextEditor from '../../components/TextEditor';
 
@@ -7,7 +12,15 @@ class Highlighter extends Component {
     render() {
         return (
             <Section title="highlighter">
-                <TextEditor />
+                <TextEditor
+                    highlightColor={this.props.highlightColor}
+                    markersColors={this.props.markersColors}
+                    currentText={this.props.textInput}
+                    onSelection={this.props.onSelection}
+                    handleSelection={this.props.handleSelection}
+                    onTextInput={this.props.onTextInput}
+                    onColorChange={this.props.onColorChange}
+                />
             </Section>
         );
     }
@@ -15,14 +28,19 @@ class Highlighter extends Component {
 
 const mapStateToProps = state => {
     return {
-        // TODO: add state
+        highlightColor: state.color.highlight_color,
+        markersColors: state.color.marker_colors,
+        textInput: state.highlight.textInput
     }
-}
+};
 
 const mapDispatchToProps = dispatch => {
     return {
-        // TODO: add dispatch actions
+        onSelection: selection => dispatch(highlightText(selection)),
+        handleSelection: note => dispatch(addNote(note)),
+        onTextInput: text => dispatch(addText(text)),
+        onColorChange: color => dispatch(setCurrentColor(color))
     }
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Highlighter);
