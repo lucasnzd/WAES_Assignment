459f37ec6e7f3cc0c8d627b357d23ea2a49b2c63
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 19:25:31 -0300

    clean up over actionTypes.js

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,16 +1,16 @@
 // Highlight_actions
-export const ADD_TEXT = 'ADD_TEXT';
-export const ADD_NOTE = 'ADD_NOTE';
-export const HIGHLIGHT_TEXT = 'HIGHLIGHT_TEXT';
+const ADD_TEXT = 'ADD_TEXT';
+const ADD_NOTE = 'ADD_NOTE';
+const HIGHLIGHT_TEXT = 'HIGHLIGHT_TEXT';
 
 // Filter_actions
-export const FILTER_BY = 'FILTER_BY';
-export const SHOW_ALL = 'FILTER_BY';
+const FILTER_BY = 'FILTER_BY';
+const SHOW_ALL = 'FILTER_BY';
 
 // Color_actions
-export const SET_CURRENT_COLOR = 'SET_CURRENT_COLOR';
+const SET_CURRENT_COLOR = 'SET_CURRENT_COLOR';
 
-export default {
+export {
     ADD_TEXT,
     ADD_NOTE,
     FILTER_BY,
