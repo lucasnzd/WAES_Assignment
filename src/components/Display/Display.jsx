a2fba88071aa297dec507b68530100c92946ba0b
Lucas Nuñez <lucas.nz.d@gmail.com>
2018-12-18 11:59:08 -0300

    Updated Display component to use props

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -3,16 +3,6 @@ import displayProps from './display.props';
 import Note from '../Note';
 import styles from './display.module.css';
 
-const notes = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
-    'Cras eu velit nec sem facilisis efficitur.',
-    'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
-    'Donec pellentesque odio at felis lacinia luctus. Phasellus bibendum efficitur faucibus.',
-    'Vestibulum augue velit, elementum ut est id, consectetur commodo velit.',
-    'Donec euismod odio at odio consequat, euismod placerat quam tincidunt.',
-    'Donec nunc nisl, gravida vitae feugiat consequat, aliquam interdum ipsum.'
-];
-
 class Display extends Component {
     constructor(props) {
         super(props);
@@ -20,8 +10,9 @@ class Display extends Component {
     }
 
     mappedNotes() {
-        // TODO: REPLACE THIS FOR THE STATE NOTES
-        return notes.map(
+        if (this.props.notes.length === 0) return;
+
+        return this.props.notes.map(
             (noteText, index) =>
                 <Note
                     key={`${this.props.color}_${index}`}
